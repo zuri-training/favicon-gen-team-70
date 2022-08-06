86699dea6c3023b1968e25562964d0ecb4bbb9aa
Adu Sarah <adusarah@Adus-Mackbook.local>
2022-08-06 15:32:26 +0100

    Working on media query

--- static/home/contact.js
+++ static/home/contact.js
@@ -34,25 +34,3 @@ function noShow(){
       isClicked = false;
     }
 }
-
-// const btn = document.getElementById("button");
-
-// document.getElementById("form").addEventListener("submit", function (event) {
-//   event.preventDefault();
-
-//   btn.value = "Sending...";
-
-//   const serviceID = "default_service";
-//   const templateID = "template_90pea1x";
-
-//   emailjs.sendForm(serviceID, templateID, this).then(
-//     () => {
-//       btn.value = "Send Email";
-//       alert("Sent!");
-//     },
-//     (err) => {
-//       btn.value = "Send Email";
-//       alert(JSON.stringify(err));
-//     }
-//   );
-// });
