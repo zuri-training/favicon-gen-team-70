d0b4f90aac51695326f13551d4c561135b960671
Jude <judetochyokoye@gmail.com>
2022-08-08 23:14:01 +0100

    finished dashboard routings and templating

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import index, saved_favicons, output, text_to_favicon
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('',index, name="index"),
+    path('savedfavicons',saved_favicons, name="saved_favicons"),
+    path('output',output, name="output"),
+    path('text-to-favicon',text_to_favicon, name="text_to_favicon"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,4 +6,36 @@ from django.contrib import messages
 
 @login_required(redirect_field_name="next", login_url="accounts:login")
 def index(request):
-    return render(request,'dashboard/index.html')
+    current_user = request.user
+    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
+    initials =  initials.upper()
+    return render(request,'dashboard/index.html',{
+        'initials':initials,
+    })
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def saved_favicons(request):
+    current_user = request.user
+    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
+    initials =  initials.upper()
+    return render(request,'dashboard/savedicons.html',{
+        'initials':initials,
+    })
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def output(request):
+    current_user = request.user
+    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
+    initials =  initials.upper()
+    return render(request,'dashboard/output.html',{
+        'initials':initials,
+    })
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def text_to_favicon(request):
+    current_user = request.user
+    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
+    initials =  initials.upper()
+    return render(request,'dashboard/text-to-favicon.html',{
+        'initials':initials,
+    })
