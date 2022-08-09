74b8398b3770d401823fab8b370534713303522a
Jude <judetochyokoye@gmail.com>
2022-08-09 17:27:15 +0100

    finished generate, zip, download and save features

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from django.http import HttpResponse, FileResponse
+import os
+from .models import Photo, Zip
+from django.utils.encoding import smart_str
 # Create your views here.
 
 @login_required(redirect_field_name="next", login_url="accounts:login")
@@ -10,15 +14,6 @@ def index(request):
     initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
     initials =  initials.upper()
     return render(request,'dashboard/index.html',{
-        'initials':initials,
-    })
-
-@login_required(redirect_field_name="next", login_url="accounts:login")
-def saved_favicons(request):
-    current_user = request.user
-    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
-    initials =  initials.upper()
-    return render(request,'dashboard/savedicons.html',{
         'initials':initials,
     })
 
@@ -39,3 +34,88 @@ def text_to_favicon(request):
     return render(request,'dashboard/text-to-favicon.html',{
         'initials':initials,
     })
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def upload(request):
+    if request.method == "POST":
+        uploaded = request.FILES['upload']
+        if uploaded.size / 1024 / 1024 > 2:
+            messages.error(request,f"{uploaded.name} exceeds 2mB, Reupload a lesser one!")
+            return redirect('dashboard:index')
+        else:
+            img_type = uploaded.content_type.split("/")[1]
+            accepted = ['jpeg','png', 'svg+xml','svg']
+            if img_type in accepted:
+                photo = Photo()
+                uploaded.name = uploaded.name.replace(" ","_")
+                print(uploaded.name)
+                photo.name = uploaded.name
+                na_me = photo.name.split(".")
+                photo.zip_name = na_me[0]
+                photo.uploader =request.user
+                photo.img_file = uploaded
+                photo.save();
+                messages.success(request,f"{uploaded.name} was uploaded successfully!")
+                return redirect('generate:generate_all',photo.id)
+            else:
+                messages.error(request,f"{uploaded.name} has an unsupported extension")
+            return redirect('dashboard:index')
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def output(request, zip_id):
+    zip_list = Zip.objects.filter(id=zip_id)
+    current_user = request.user
+    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
+    initials =  initials.upper()
+    if zip_list.exists():
+        zip = zip_list[0]
+        if 'download' in request.POST:
+            return redirect('dashboard:download',zip.id)
+        elif 'save' in request.POST:
+            zip.is_saved = True
+            zip.save();
+            messages.success(request,f"{zip.name} was saved successfully !")
+            return redirect('dashboard:saved_favicons')
+        return render(request, 'dashboard/output.html',{
+            'zip':zip,
+            'initials':initials,
+        })   
+    return render(request,'error/error.html')
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def saved_favicons(request):
+    zips = Zip.objects.filter(downloader=request.user, is_saved=True)
+    current_user = request.user
+    initials = f"{current_user.first_name[0]}{current_user.last_name[0]}"
+    initials =  initials.upper()
+    if len(zips) >= 1:
+        return render(request,'dashboard/savedicons.html', {
+            'zips':zips,
+            'initials':initials,
+        })
+    else:
+        return render(request,'dashboard/emptyCollections.html',{
+            'initials':initials,
+        })
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def delete_view(request,pk):
+    if request.method == "POST":
+        del_zip =Zip.objects.get(pk=pk)
+        del_zip.delete();
+    return redirect('dashboard:saved_favicons')
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def save_view(request,zip_id):
+    if request.method == "POST":
+        save_zip =Zip.objects.get(id=zip_id)
+        save_zip.is_saved = True
+        save_zip.save();
+    return redirect('dashboard:saved_favicons')
+
+@login_required(redirect_field_name="next", login_url="accounts:login")
+def download(request,zip_id):
+    if request.method == "POST":
+        download_zip = Zip.objects.get(id=zip_id)
+        return FileResponse(download_zip.zip_file, as_attachment=True)
+    return redirect('dashboard:index')
